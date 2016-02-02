68ab8ec4edcec2d4c9300cd3d5c9e457425fe428
villj232 <javiervillegas@javiers-MBP.wireless.newschool.edu>
2016-02-02 16:54:09 -0500

    done

--- sketch.js
+++ sketch.js
@@ -5,7 +5,61 @@ function setup(){
 }
 
 function draw(){
-  background("cadetblue");
+  background("black");
 
-  ellipse(windowWidth/2,windowHeight/2,100,100);
+  fill("#FFD700");
+  rect(400,100,300,500,10);
+  fill("black");
+  rect(420,120,260,230,10);
+  fill("#778899");
+  rect(460,160,180,130);
+  rect(510,370,80,15,20);
+  fill("black");
+  rect(420,438,80,25);
+  rect(448,410,25,80);
+  fill("#303030");
+  triangle(451,430,461,415,471,430);
+  triangle(451,472,461,487,471,472);
+  triangle(437,461,425,451,437,441);
+  triangle(484,461,497,451,484,441);
+  fill("red");
+  ellipse(430,200,8,8)
+  fill("black");
+  ellipse(610,470,30,30);
+  ellipse(660,450,30,30);
+  fill("#303030");
+  ellipse(610,470,20,20);
+  ellipse(660,450,20,20);
+  ellipse(530,540,30,10);
+  ellipse(580,540,30,10);
+  fill("black");
+  ellipse(640,550,6,6);
+  ellipse(660,550,6,6);
+  ellipse(680,550,6,6);
+  ellipse(630,560,6,6);
+  ellipse(650,560,6,6);
+  ellipse(670,560,6,6);
+  ellipse(690,560,6,6);
+  ellipse(640,570,6,6);
+  ellipse(660,570,6,6);
+  ellipse(680,570,6,6);
+  ellipse(630,580,6,6);
+  ellipse(650,580,6,6);
+  ellipse(670,580,6,6);
+  ellipse(690,580,6,6);
+  ellipse(640,590,6,6);
+  ellipse(660,590,6,6);
+  ellipse(680,590,6,6);
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ sketch2.js
@@ -0,0 +1,11 @@
+//Your Drawing
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+  background("cadetblue");
+
+  ellipse(windowWidth/2,windowHeight/2,100,100);
+}
